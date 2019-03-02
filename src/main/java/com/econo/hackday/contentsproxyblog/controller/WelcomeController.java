3ca9dd9f13893d3d048bf755f63396a6c99e970c
dadadamarine <dadadamarine@gmail.com>
2019-03-02 16:44:21 +0900

    add : index 페이지 redirect 구현 및 테스트 코드 작 (#26)

---
+++ src/main/java/com/econo/hackday/contentsproxyblog/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.econo.hackday.contentsproxyblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping
+	public String getWelcome() {
+		return "redirect:/posts";
+	}
+
+
+}
