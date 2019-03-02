63ee798921d2bef06ba04e0e345500acd1ff630f
dadadamarine <dadadamarine@gmail.com>
2019-03-02 15:20:59 +0900

    fix : GithubMarkdownLoader테스트 수정 1. 스프링과 함께 테스트하던 불필요한 과정 생략 2. 레거시 코드 제거 (#8)

--- src/test/java/com/econo/hackday/contentsproxyblog/utils/GithubMarkdownLoaderTest.java
+++ src/test/java/com/econo/hackday/contentsproxyblog/utils/GithubMarkdownLoaderTest.java
@@ -1,29 +1,16 @@
 package com.econo.hackday.contentsproxyblog.utils;
 
-import org.eclipse.egit.github.core.RepositoryId;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.egit.github.core.RepositoryId.createFromUrl;
 
-@RunWith(SpringRunner.class)
 public class GithubMarkdownLoaderTest {
-
-    GithubMarkdownLoader githubMarkdownLoader;
-
-    public GithubMarkdownLoaderTest() {
-        githubMarkdownLoader = new GithubMarkdownLoader();
-    }
 
     @Test
     public void getContentsTest() throws IOException {
-        String uri = "https://github.com/JNU-econovation/contents-proxy-blog-team1/blob/README.md";
+        String uri = "https://github.com/JNU-econovation/contents-proxy-blog-team1/blob/master/README.md";
         String answer = "## 기술 스택\n" +
                 "- Java 11\n" +
                 "- Web tier : Spring Boot (+ Spring Web MVC)\n" +
@@ -34,18 +21,13 @@ public class GithubMarkdownLoaderTest {
                 "    - 클라이언트에서 할 경우 : [markdown-it](https://github.com/markdown-it/markdown-it)\n" +
                 "- DB : H2db\n\n";
 
-        assertThat(githubMarkdownLoader.getContents(uri)).contains(answer);
+        assertThat(GithubMarkdownLoader.getContents(uri)).contains(answer);
     }
 
     @Test
-    public void getFilePathTest() throws MalformedURLException {
-        String uri = "https://github.com/JNU-econovation/contents-proxy-blog-team1/blob/test/README.md";
-        System.out.println("? : " + new URL(uri).getPath());
-        RepositoryId repositoryId = createFromUrl(uri);
-        RepositoryId repositoryId1 = createFromUrl(new URL(uri));
-        System.out.println(repositoryId.getOwner());
-        System.out.println(repositoryId1.getOwner());
+    public void getFilePathTest() {
+        String uri = "https://github.com/JNU-econovation/contents-proxy-blog-team1/blob/master/README.md";
 
-        assertThat(githubMarkdownLoader.getFilePath(uri)).isEqualTo("/test/README.md");
+        assertThat(GithubMarkdownLoader.getFilePath(uri)).isEqualTo("/README.md");
     }
 }
